6511c63f640b8dbbb2d109ebfc9cf3836ba69488
Alvin Awuor <alvinawuor8@gmail.com>
2021-04-18 10:56:23 +0300

    Add method to Test if user object is saved into user list.

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import unittest
 from user_credentials import User, Credentials
 
-class TestContact(unittest.TestCase):
+class TestUser(unittest.TestCase):
 
     """
     Test class that defines test cases for the user and credential class behaviours.
@@ -17,7 +17,7 @@ class TestContact(unittest.TestCase):
         User.user_list = []
         Credentials.credential_list = []
 
-    def user_set_up(self):
+    def setUp(self):
         """
         Set up method to run before each test case.
         """
@@ -25,11 +25,22 @@ class TestContact(unittest.TestCase):
         self.new_user = User("John", "Doe", "jdoe", "jaribu1")
 
     def test_init(self):
-        '''
+        """
         Test if the object is initialized properly.
-        '''
+        """
 
         self.assertEqual(self.new_user.fname, "John")
         self.assertEqual(self.new_user.lname, "Doe")
         self.assertEqual(self.new_user.username, "jdoe")
         self.assertEqual(self.new_user.password, "jaribu1")
+
+    def test_save_user(self):
+        """
+        Test if the user object is saved into the user list.
+        """
+
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
+
+if __name__ == '__main__':
+    unittest.main()
